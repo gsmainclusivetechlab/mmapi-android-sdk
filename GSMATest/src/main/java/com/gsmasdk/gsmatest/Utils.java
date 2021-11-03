70792f4ab7df529d9be619b7539f59072e419e8a
Hareesh Menon <hareesh.menon@fingent.com>
2021-11-03 23:31:09 +0530

    - updated UI and toast messages for test app

---
+++ GSMATest/src/main/java/com/gsmasdk/gsmatest/Utils.java
@@ -0,0 +1,25 @@
+package com.gsmasdk.gsmatest;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.text.Html;
+import android.view.Gravity;
+import android.widget.Toast;
+
+public class Utils {
+
+    // helper method for showing toast.
+    public static void showToast(Context context, String message) {
+        Toast toast = Toast.makeText(context, Html.fromHtml("<font color='#3700B3' ><b>" + message + "</b></font>"), Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.TOP, 0, 0);
+        toast.show();
+    }
+
+    //helper method for Progress dialogs
+    public static ProgressDialog initProgress(Context context){
+        ProgressDialog  progressdialog=new ProgressDialog(context,R.style.MyAlertDialogStyle);
+        progressdialog.setMessage("Please wait");
+        progressdialog.setCancelable(false);
+        return progressdialog;
+    }
+}
