a7fb1ed41c7e5651b1d21cb6d63c5a8b67a60bd9
anandhu.kalesh <anandhu.kalesh@fingent.com>
2021-12-01 16:12:40 +0530

    Add common functions

--- GSMASdk/src/main/java/com/gsmaSdk/gsma/controllers/AccountLinking.java
+++ GSMASdk/src/main/java/com/gsmaSdk/gsma/controllers/AccountLinking.java
@@ -1,4 +1,53 @@
 package com.gsmaSdk.gsma.controllers;
 
+import androidx.annotation.NonNull;
+
+import com.gsmaSdk.gsma.interfaces.BalanceInterface;
+import com.gsmaSdk.gsma.interfaces.RequestStateInterface;
+import com.gsmaSdk.gsma.interfaces.RetrieveTransactionInterface;
+import com.gsmaSdk.gsma.models.Identifier;
+import com.gsmaSdk.gsma.models.transaction.ReversalObject;
+
+import java.util.ArrayList;
+
 public class AccountLinking extends  Common{
+
+    /**
+     * Reversal - provides transaction reversal
+     *
+     * @param notificationMethod The enumerated datatype to determine polling or callback
+     * @param callbackUrl        The server URl for recieving response of transaction
+     * @param referenceId        Reference id of a previous transaction
+     * @param reversal           Reversal Object containing the type of the transaction
+     */
+
+    public void createReversal(@NonNull Enum notificationMethod, @NonNull String callbackUrl, @NonNull String referenceId, @NonNull ReversalObject reversal, @NonNull RequestStateInterface requestStateInterface) {
+        ReversalTransaction.getInstance().createReversal(notificationMethod,callbackUrl,referenceId,reversal,requestStateInterface);
+
+    }
+
+    /**
+     * View Account Balance-Get the balance of a particular account
+     *
+     * @param identifierArrayList - List of identifiers to identify a particular account
+     */
+    public void viewAccountBalance(@NonNull ArrayList<Identifier> identifierArrayList, @NonNull BalanceInterface balanceInterface) {
+        AccountBalance.getInstance().viewAccountBalance(identifierArrayList,balanceInterface);
+
+    }
+
+    /**
+     * Retrieve a transaction
+     *
+     * @param identifierArrayList List of account identifiers of a user
+     * @param offset              offset required for pagination
+     * @param limit               limit set for receiving records per request
+     */
+    public void viewAccountTransactions(@NonNull ArrayList<Identifier> identifierArrayList, int offset, int limit, @NonNull RetrieveTransactionInterface retrieveTransactionInterface) {
+        AccountTransactions.getInstance().viewAccountTransactions(identifierArrayList,offset,limit,retrieveTransactionInterface);
+
+    }
+
+
+
 }
--- GSMATest/src/main/java/com/gsmasdk/gsmatest/AccountLinkingActivity.java
+++ GSMATest/src/main/java/com/gsmasdk/gsmatest/AccountLinkingActivity.java
@@ -12,13 +12,23 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.gsmaSdk.gsma.enums.NotificationMethod;
+import com.gsmaSdk.gsma.interfaces.BalanceInterface;
+import com.gsmaSdk.gsma.interfaces.MissingResponseInterface;
+import com.gsmaSdk.gsma.interfaces.RequestStateInterface;
+import com.gsmaSdk.gsma.interfaces.RetrieveTransactionInterface;
 import com.gsmaSdk.gsma.interfaces.ServiceAvailabilityInterface;
+import com.gsmaSdk.gsma.interfaces.TransactionInterface;
 import com.gsmaSdk.gsma.manager.SDKManager;
 import com.gsmaSdk.gsma.models.Identifier;
+import com.gsmaSdk.gsma.models.MissingResponse;
+import com.gsmaSdk.gsma.models.common.Balance;
 import com.gsmaSdk.gsma.models.common.ErrorObject;
 import com.gsmaSdk.gsma.models.common.GSMAError;
+import com.gsmaSdk.gsma.models.common.RequestStateObject;
 import com.gsmaSdk.gsma.models.common.ServiceAvailability;
 import com.gsmaSdk.gsma.models.transaction.ReversalObject;
+import com.gsmaSdk.gsma.models.transaction.Transaction;
 import com.gsmaSdk.gsma.models.transaction.TransactionRequest;
 
 import java.util.ArrayList;
@@ -69,8 +79,14 @@ public class AccountLinkingActivity extends AppCompatActivity implements Adapter
         txtResponse = findViewById(R.id.txtAccountLinkResponse);
         txtResponse.setMovementMethod(new ScrollingMovementMethod());
         progressdialog = Utils.initProgress(AccountLinkingActivity.this);
+
         checkServiceAvailability();
+        createPaymentReversalObject();
+        createAccountIdentifier();
+
     }
+
+
 
 
     @Override
@@ -88,28 +104,55 @@ public class AccountLinkingActivity extends AppCompatActivity implements Adapter
                        //View a Account Link
                         break;
                     case 3:
-                        //Perform a Transfer Reversal
+                        //Perform a Transfer for a Linked Account
 
                         break;
                     case 4:
                       //View Transaction
+                        viewTransaction();
+
                         break;
                     case 5:
                        //reversal
+                        paymentReversal();
                         break;
                     case 6:
                         //Obtain a Financial Service Provider Balance
-
+                        balanceCheck();
                         break;
                     case 7:
                         // Retrieve Transfers for a Financial Service Provider
+                        retrieveTransactionFSP();
 
                     case 8:
                         // Retrieve a Missing API Response
+                        getMissingTransaction();
                     default:
                         break;
          }
     }
+    /**
+     * Create Payment Reversal Object.
+     */
+    private void createPaymentReversalObject() {
+        reversalObject = new ReversalObject();
+        reversalObject.setReversal("reversal");
+    }
+
+
+    private void createAccountIdentifier() {
+        identifierArrayList = new ArrayList<>();
+        identifierArrayList.clear();
+
+        //account id
+        Identifier identifierAccount = new Identifier();
+        identifierAccount.setKey("accountid");
+        identifierAccount.setValue("2000");
+
+        identifierArrayList.add(identifierAccount);
+    }
+
+
 
     /**
      * Method for checking Service Availability.
@@ -143,6 +186,171 @@ public class AccountLinkingActivity extends AppCompatActivity implements Adapter
     }
 
 
+    /**
+     * View Transaction-View the transaction Details
+     */
+    private void viewTransaction() {
+        showLoading();
+        SDKManager.accountLinking.viewTransaction(transactionRef, new TransactionInterface() {
+            @Override
+            public void onValidationError(ErrorObject errorObject) {
+                hideLoading();
+                Utils.showToast(AccountLinkingActivity.this, errorObject.getErrorDescription());
+                Log.d(VALIDATION, "onValidationError: " + new Gson().toJson(errorObject));
+            }
+
+            @Override
+            public void onTransactionSuccess(TransactionRequest transactionRequest) {
+                hideLoading();
+                txtResponse.setText(new Gson().toJson(transactionRequest));
+                Utils.showToast(AccountLinkingActivity.this, "Success");
+                Log.d(SUCCESS, "onTransactionSuccess: " + new Gson().toJson(transactionRequest));
+            }
+
+            @Override
+            public void onTransactionFailure(GSMAError gsmaError) {
+                hideLoading();
+                Utils.showToast(AccountLinkingActivity.this, "Failure");
+                txtResponse.setText(new Gson().toJson(gsmaError));
+                Log.d(FAILURE, "onTransactionFailure: " + new Gson().toJson(gsmaError));
+            }
+
+        });
+    }
+
+    //Retrieve Transaction for an FSP
+    private void retrieveTransactionFSP() {
+        showLoading();
+        SDKManager.accountLinking.viewAccountTransactions(identifierArrayList, 0, 2, new RetrieveTransactionInterface() {
+            @Override
+            public void onValidationError(ErrorObject errorObject) {
+                hideLoading();
+                Utils.showToast(AccountLinkingActivity.this, errorObject.getErrorDescription());
+                Log.d(VALIDATION, "onValidationError: " + new Gson().toJson(errorObject));
+            }
+
+            @Override
+            public void onRetrieveTransactionSuccess(Transaction transaction) {
+                hideLoading();
+                Utils.showToast(AccountLinkingActivity.this, "Success");
+                txtResponse.setText(new Gson().toJson(transaction));
+                Log.d(SUCCESS, "onRetrieveTransactionSuccess: " + new Gson().toJson(transaction));
+            }
+
+            @Override
+            public void onRetrieveTransactionFailure(GSMAError gsmaError) {
+                hideLoading();
+                txtResponse.setText(new Gson().toJson(gsmaError));
+                Log.d(FAILURE, "onRetrieveTransactionFailure: " + new Gson().toJson(gsmaError));
+            }
+        });
+    }
+
+
+
+    /**
+     * Payment Reversal
+     */
+    private void paymentReversal() {
+        showLoading();
+        SDKManager.accountLinking.createReversal(NotificationMethod.POLLING, "", "REF-1633580365289", reversalObject, new RequestStateInterface() {
+            @Override
+            public void onRequestStateSuccess(RequestStateObject requestStateObject) {
+                hideLoading();
+                Utils.showToast(AccountLinkingActivity.this, "Success");
+                serverCorrelationId = requestStateObject.getServerCorrelationId();
+                txtResponse.setText(new Gson().toJson(requestStateObject));
+                Log.d(SUCCESS, "onReversalSuccess:" + new Gson().toJson(requestStateObject));
+            }
+
+            @Override
+            public void onRequestStateFailure(GSMAError gsmaError) {
+                hideLoading();
+                Utils.showToast(AccountLinkingActivity.this, "Failure");
+                txtResponse.setText(new Gson().toJson(gsmaError));
+                hideLoading();
+                Log.d(FAILURE, "onReversalFailure: " + new Gson().toJson(gsmaError));
+            }
+
+            @Override
+            public void onValidationError(ErrorObject errorObject) {
+                hideLoading();
+                Utils.showToast(AccountLinkingActivity.this, errorObject.getErrorDescription());
+                Log.d(VALIDATION, "onValidationError: " + new Gson().toJson(errorObject));
+            }
+
+            @Override
+            public void getCorrelationId(String correlationID) {
+                correlationId = correlationID;
+                Log.d("getCorrelationId", "correlationId: " + correlationID);
+            }
+
+        });
+    }
+
+
+    //Check Balance
+    private void balanceCheck() {
+        showLoading();
+        SDKManager.accountLinking.viewAccountBalance(identifierArrayList, new BalanceInterface() {
+            @Override
+            public void onValidationError(ErrorObject errorObject) {
+                hideLoading();
+                Utils.showToast(AccountLinkingActivity.this, errorObject.getErrorDescription());
+                Log.d(VALIDATION, "onValidationError: " + new Gson().toJson(errorObject));
+            }
+
+            @SuppressWarnings("unused")
+            @Override
+            public void onBalanceSuccess(Balance balance) {
+                hideLoading();
+                Utils.showToast(AccountLinkingActivity.this, "Success");
+                txtResponse.setText(new Gson().toJson(balance));
+                Log.d(SUCCESS, "onBalanceSuccess: " + new Gson().toJson(balance));
+            }
+
+            @SuppressWarnings("unused")
+            @Override
+            public void onBalanceFailure(GSMAError gsmaError) {
+                hideLoading();
+                txtResponse.setText(new Gson().toJson(gsmaError));
+                Log.d(FAILURE, "onBalanceFailure: " + new Gson().toJson(gsmaError));
+            }
+        });
+    }
+
+    //Retrieve a missing Transaction
+    private void getMissingTransaction() {
+        showLoading();
+
+        SDKManager.accountLinking.viewResponse(correlationId, new MissingResponseInterface() {
+            @Override
+            public void onMissingResponseSuccess(MissingResponse missingResponse) {
+                hideLoading();
+                Utils.showToast(AccountLinkingActivity.this, "Success");
+                txtResponse.setText(new Gson().toJson(missingResponse));
+                Log.d(SUCCESS, "onMissingTransactionSuccess: " + new Gson().toJson(missingResponse));
+            }
+
+            @Override
+            public void onMissingResponseFailure(GSMAError gsmaError) {
+                hideLoading();
+                txtResponse.setText(new Gson().toJson(gsmaError));
+                Utils.showToast(AccountLinkingActivity.this, "Failure");
+                Log.d(FAILURE, "onTransactionFailure: " + new Gson().toJson(gsmaError));
+            }
+
+            @Override
+            public void onValidationError(ErrorObject errorObject) {
+                hideLoading();
+                Utils.showToast(AccountLinkingActivity.this, errorObject.getErrorDescription());
+                Log.d(VALIDATION, "onValidationError: " + new Gson().toJson(errorObject));
+            }
+        });
+
+
+    }
+
     public void showLoading() {
         progressdialog.show();
     }
@@ -150,5 +358,7 @@ public class AccountLinkingActivity extends AppCompatActivity implements Adapter
     public void hideLoading() {
         progressdialog.dismiss();
     }
+
+
 
 }
