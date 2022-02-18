244e35a169dc349c900743dfccc9bb9ce7c9e9f1
jerin <jerin.babu@fingent.com>
2022-02-18 18:52:36 +0530

    UI Enhancement

---
+++ GSMATest/src/main/java/com/gsmasdk/gsmatest/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.gsmasdk.gsmatest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this, LandingActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
